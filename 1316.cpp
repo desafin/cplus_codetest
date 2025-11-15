d136fdef095a34faeba6af3d115ddc2bd4f9d518
root <root@DESKTOP-VLD4HHD.localdomain>
2025-11-16 07:41:53 +0900

    add

--- 1316.cpp
+++ 1316.cpp
@@ -1,7 +1,23 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 	// 코드 작성
+	int>>num;
+	for(int i=0;i<num;i++)
+	{
+		string str1;
+		cin>>str1;
+		for(int i=1;i<str1.size();i++)
+		{
+			if(str1[i-1]==str1[i])
+			{
+				
+			}
+		}
+	}
 	return 0;
 }
