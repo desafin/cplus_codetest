e4ee5215677b61ffd13b3e1f7c65d6393c7a1851
Sangwon Moon <desafinado12@hotmail.com>
2026-01-09 07:38:03 +0900

    add 01 09

--- 2920.cpp
+++ 2920.cpp
@@ -8,5 +8,53 @@ int main() {
   {
 	int num;
 	cin>>num;
+  v.push_back(num);
   }
+
+  int min =1;
+  for(int i=0;i<8;i++)
+  {
+    if(v[i]==min)
+    {
+      min++;
+      if(min==8)
+      {
+        cout<<"ascending"<<endl;
+        return 0;
+      }
+      else
+      {
+        continue;
+
+      }
+    }
+    else
+    {
+      break;
+    }
+  }
+  int max =8;
+  for(int i=0;i<8;i++)
+  {
+    if(v[i]==max)
+    {
+      max--;
+      if(max==1)
+      {
+        cout<<"descending"<<endl;
+        return 0;
+      }
+      else
+      {
+        continue;
+
+      }
+    }
+    else
+    {
+      break;
+    }
+  }
+  cout<<"mixed"<<endl;
+  return 0;
 }
