21d7a6f0bf8e8fe5a09d12f102433e5c69b8454b
Sangwon Moon <desafinado12@hotmail.com>
2026-01-11 20:25:50 +0900

    add 01 11

--- 1874.cpp
+++ 1874.cpp
@@ -2,22 +2,44 @@
 using namespace std;
 
 int main() {
-	iso_base::sync_with_stdio(0);
-	cin.tie(0);
-	stack<int> stack;
-	
-	//오름차순대로 들어가도록 push가 목표 하지만 중간에 순서를 바꿔줘야함 
-	int num;
-	cin>>num;
-	//최대값 선언해야함
-	for(int i=0;i<num;i++)
-	{
-		int n;
-		cin>>n;
-		//오름차순일때만 push 즉 top이랑 비교해야함
-		stack.push(n);
-	}
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
 
-	// 코드 작성
-	return 0;
-} y
+  stack<int> s;
+  vector<char> result;
+
+  int num;
+  cin >> num;
+
+  int cnt = 1;
+  bool possible = true;
+
+  for (int i = 0; i < num; i++) {
+    int x;
+    cin >> x;
+
+    while (cnt <= x) {
+      s.push(cnt);
+      cnt++;
+      result.push_back('+');
+    }
+
+    if (s.top() == x) {
+      s.pop();
+      result.push_back('-');
+    } else {
+      possible = false;
+      break;
+    }
+  }
+
+  if (possible) {
+    for (int i = 0; i < result.size(); i++) {
+      cout << result[i] << "\n";
+    }
+  } else {
+    cout << "NO";
+  }
+
+  return 0;
+}
